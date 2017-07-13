f2609616dd5e84b9737a4475c4e4f99f01ec809f
epikjjh <epikjjh@gmail.com>
2017-07-13 20:28:22 +0900

    blynk example code

--- blynk/example/example.ino
+++ blynk/example/example.ino
@@ -1,36 +1,76 @@
+/**************************************************************
+ * Blynk is a platform with iOS and Android apps to control
+ * Arduino, Raspberry Pi and the likes over the Internet.
+ * You can easily build graphic interfaces for all your
+ * projects by simply dragging and dropping widgets.
+ *
+ *   Downloads, docs, tutorials: http://www.blynk.cc
+ *   Blynk community:            http://community.blynk.cc
+ *   Social groups:              http://www.fb.com/blynkapp
+ *                               http://twitter.com/blynk_app
+ *
+ * Blynk library is licensed under MIT license
+ * This example code is in public domain.
+ *
+ **************************************************************
+ * This example shows how to use ordinary Arduino Serial
+ * to connect your project to Blynk.
+ * Feel free to apply it to any other example. It's simple!
+ *
+ * 1. Optional, but recommended.
+ *    Connect additional USB-serial adapter to see the prints.
+ *
+ * 2. Edit auth token and upload this sketch.
+ *
+ * 3. Run the script (script located in "scripts" folder of library root,
+ *    e.g. 'blynk-library/scripts') for redirecting traffic to server:
+ *
+ *      for Windows:         scripts/blynk-ser.bat
+ *      for Linux and OSX:   ./scripts/blynk-ser.sh (may need to run with sudo)
+ *
+ *    You can specify port, baud rate, and server endpoint like this:
+ *      ./blynk-ser.sh -c <serial port> -b <baud rate> -s <server address> -p <server port>
+ *
+ *    For instance :
+ *      ./blynk-ser.sh -c /dev/ttyACM0 -b 9600 -s cloud.blynk.cc -p 8442
+ *
+ *    Run blynk-ser.sh -h for more information
+ *
+ *    Be sure to select the right serial port (there may be multiple).
+ *
+ *    Attention!
+ *        Arduino IDE may complain with "programmer is not responding".
+ *        You need to terminate script before uploading new sketch.
+ *
+ * 4. Start blynking! :)
+ *
+ **************************************************************/
+ 
+/* Enter this route to use blynk connection
+// C:\Program Files (x86)\Arduino\libraries\blynk-library-0.2.1\scripts\
+// Enter blynk-ser.bat -c COM5 -b 9600 -s cloud.blynk.cc -p 8442 in your bash
+// COM(Number) can change by your machine
+*/
 
-int trig_pin = 2;
-int echo_pin = 3;
-float duration, distance;
+// You could use a spare Hardware Serial on boards that have it (like Mega)
+#include <SoftwareSerial.h>
+SoftwareSerial SwSerial(10, 11);
+#define BLYNK_PRINT SwSerial
+#include <BlynkSimpleSerial.h>
 
-void setup() {
-  // Setting for serial connection.
-  Serial.begin(9600);
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "1d1fcf07ffaf4c8198a7a7db3c95d921";
 
-  // Setting for input,output
-  pinMode(trig_pin, OUTPUT);
-  pinMode(echo_pin, INPUT);
-
+void setup()
+{
+  SwSerial.begin(9600);
+  Blynk.begin(auth);
+  // Default baud rate is 9600. You could specify it like this:
+  //Blynk.begin(auth, 57600);
 }
 
-void loop() {
-  digitalWrite(trig_pin, HIGH);
-  // Delay time : 1 sec
-  delay(1000);
-  digitalWrite(trig_pin, LOW);
-
-  // Write duration of high state at echo pin.
-  duration = pulseIn(echo_pin, HIGH);
-  
-  // Ultrasonic velocity : 340 m/s, Think about round trip
-  distance = ((float)(340*duration)/10000)/2;
-
-  // Demo distance : 10cm
-  if(distance < 10){
-    Serial.print("Open!\n");
-  }
-  else{
-    Serial.print("Close!\n");
-  }
-  delay(3000);
+void loop()
+{
+  Blynk.run();
 }
