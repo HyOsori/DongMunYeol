26cc3cc05ae51f1a73f3b644b4ba728527e40fb6
epikjjh <epikjjh@gmail.com>
2017-07-13 18:17:53 +0900

    Ultrasonic sensor example

---
+++ ultrasonic sensor/example/example.ino
@@ -0,0 +1,36 @@
+
+int trig_pin = 2;
+int echo_pin = 3;
+float duration, distance;
+
+void setup() {
+  // Setting for serial connection.
+  Serial.begin(9600);
+
+  // Setting for input,output
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+
+}
+
+void loop() {
+  digitalWrite(trig_pin, HIGH);
+  // Delay time : 1 sec
+  delay(1000);
+  digitalWrite(trig_pin, LOW);
+
+  // Write duration of high state at echo pin.
+  duration = pulseIn(echo_pin, HIGH);
+  
+  // Ultrasonic velocity : 340 m/s, Think about round trip
+  distance = ((float)(340*duration)/10000)/2;
+
+  // Demo distance : 10cm
+  if(distance < 10){
+    Serial.print("Open!\n");
+  }
+  else{
+    Serial.print("Close!\n");
+  }
+  delay(3000);
+}
