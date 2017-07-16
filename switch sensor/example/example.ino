e8be3da47e298d188616c60e306d777614f26e6c
JunwonP <merlin0876@gmail.com>
2017-07-16 16:29:47 +0900

    Switch sensor example

--- switch sensor/example/example.ino
+++ switch sensor/example/example.ino
@@ -1,36 +1,41 @@
+//See attached jpeg
+// plug button into digital pin 2
+int btn = 2;
+// plug Green LED into digital pin 3 
+int led_G = 3; 
+// plug Red LED into digital pin 4
+int led_R = 4; 
 
-int trig_pin = 2;
-int echo_pin = 3;
-float duration, distance;
+//init the value
+int nowState = 0;
+int oldState = 0;
+int btnState = 0;
 
 void setup() {
-  // Setting for serial connection.
-  Serial.begin(9600);
-
-  // Setting for input,output
-  pinMode(trig_pin, OUTPUT);
-  pinMode(echo_pin, INPUT);
-
+    pinMode(btn, INPUT);
+    pinMode(led_G, OUTPUT);
+    pinMode(led_R, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(trig_pin, HIGH);
-  // Delay time : 1 sec
-  delay(1000);
-  digitalWrite(trig_pin, LOW);
-
-  // Write duration of high state at echo pin.
-  duration = pulseIn(echo_pin, HIGH);
-  
-  // Ultrasonic velocity : 340 m/s, Think about round trip
-  distance = ((float)(340*duration)/10000)/2;
-
-  // Demo distance : 10cm
-  if(distance < 10){
-    Serial.print("Open!\n");
+  //When you press the button, the value of 'nowState' will be 1
+  nowState = digitalRead(btn);
+  if(nowState != oldState) {
+    if(nowState == 1) {
+      if(btnState == 0) {
+        //Red LED is on
+        digitalWrite(led_R, HIGH);
+        digitalWrite(led_G, LOW);
+        btnState = 1;
+      }
+      else {
+        //Green LED is on
+        digitalWrite(led_R,LOW);
+        digitalWrite(led_G,HIGH);
+        btnState = 0;
+      }
+    }
+    oldState = nowState;
   }
-  else{
-    Serial.print("Close!\n");
-  }
-  delay(3000);
+    delay(50);
 }
