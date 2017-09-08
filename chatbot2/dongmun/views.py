6c30abf00e42dbee78152f50e4066d02373f0039
Julia <juliajungah@gmail.com>
2017-09-08 15:51:44 +0900

    modified chatbot2 views.py

--- chatbot2/dongmun/views.py
+++ chatbot2/dongmun/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def keyboard(request):
+
+	return JsonResponse( {
+	'type' : 'buttons',
+	'buttons' : ['one', 'two']
+	})
