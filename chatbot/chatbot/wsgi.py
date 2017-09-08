03a1db1cf3165515e35abbcd8a5c05f5dfdc571a
Julia <juliajungah@gmail.com>
2017-09-08 12:33:53 +0900

    path edited wsgi.py

--- chatbot/chatbot/wsgi.py
+++ chatbot/chatbot/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os, sys
 
 sys.path.append('./')
-sys.path.append('./venv/lib/python3.6/site-packages')
+sys.path.append('./chat_venv/lib/python3.6/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
