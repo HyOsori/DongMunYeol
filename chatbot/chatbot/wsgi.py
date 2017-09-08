d959472f30c863aabfe7c94cfbe1b3e58a5fb4b0
Julia <juliajungah@gmail.com>
2017-09-08 12:19:42 +0900

    edited wsgi.py path included

--- chatbot/chatbot/wsgi.py
+++ chatbot/chatbot/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('./')
+sys.path.append('./venv/lib/python3.6/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
