dda4d5b8a91dfbd9f4df5fa939e3b5849967be43
JunwonP <merlin0876@gmail.com>
2017-09-08 13:12:15 +0900

    Modify answer/views.py

--- chatbot/answer/views.py
+++ chatbot/answer/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+def keyboard(request):
+
+    return JsonResponse({
+        'type' : 'buttons',
+        'buttons' : ['1','2']
+        })
 
 # Create your views here.
