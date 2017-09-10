6664af59530fb70281ead1e023f8ec5a42e9ae42
seubseub <seub0730@gmail.com>
2017-09-11 00:11:10 +0900

    Add post request to Ultra Sensor's code

--- ultrasonic sensor/example/example.ino
+++ ultrasonic sensor/example/example.ino
@@ -1,20 +1,30 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = { 0x90, 0x9F, 0x33, 0x79, 0x8B, 0x9D }; //physical mac address
+
+EthernetClient client;
+EthernetServer server(80);
+String data;
 int lastState=0;
 int trig_pin = 2;
 int echo_pin = 3;
 float duration, distance;
 
 void setup() {
-  // Setting for serial connection.
-  Serial.begin(9600);
 
-  // Setting for input,output
+  Serial.begin(9600);
   pinMode(trig_pin, OUTPUT);
   pinMode(echo_pin, INPUT);
+  if(Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+  }
 
+  data = "";
 }
 
 void loop() {
-  digitalWrite(trig_pin, HIGH);
+   digitalWrite(trig_pin, HIGH);
   // Delay time : 1 sec
   delay(1000);
   digitalWrite(trig_pin, LOW);
@@ -36,12 +46,40 @@ void loop() {
 
   if(currentState!=lastState){
     if(currentState==1)
-      Serial.print("Open\n");
+      postStatusData("1");
     else
-      Serial.print("Close\n");
+      postStatusData("0");
   }
   
   lastState=currentState;
   
   delay(100);
 }
+
+void postStatusData(String value) {
+  Serial.print(value);
+  String data= "{\"status\":" + value + "}";
+
+  Serial.println(Ethernet.localIP());
+  if(client.connect("211.249.63.229", 1028)) {
+    Serial.println(data);
+    client.println("POST /receive HTTP/1.1");
+    client.println("Host:  211.249.63.229:1028");
+    client.println("User-Agent: Arduino/1.0");
+    client.println("Connection: close");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.print(data);    
+    Serial.println("We have connection");
+  } else {
+    Serial.println("adsfasfadfasfasdfdas");
+  }
+
+  if(client.connected()) {    
+    Serial.println("We have abc");
+    client.stop();
+  }
+}
+
