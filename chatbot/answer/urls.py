1751a2ed9cd69fec017c8c9ab5040ba1cfe04392
epikjjh <epikjjh@gmail.com>
2017-09-10 19:57:44 +0900

    Django chatbot server

--- chatbot/answer/urls.py
+++ chatbot/answer/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^keyboard', views.keyboard),
-        url(r'^message', views.message),
+    url(r'^keyboard', views.keyboard),
+    url(r'^message', views.message),
+    url(r'^receive', views.receive),
 ]
 
--- chatbot/answer/views.py
+++ chatbot/answer/views.py
@@ -1,24 +1,24 @@
 from django.http import JsonResponse
-import serial
-from serial import SerialException
 
+isopen = 0
 
 def keyboard(request):
-
     return JsonResponse({
         'type' : 'buttons',
         'buttons' : ['DongMunYeol?']
         })
 
 def message(request):
-    T = serial.Serial('COM5', 9600)
-    result = T.readliune()
-    if(result == 1):
+    if(isopen == 1):
         return JsonResponse({
             "text" : "Open!"
         })
-    elif(result == 0):
+    elif(isopen == 0):
         return JsonResponse({
             "text" : "Close!"
         })
+
+
+def receive(request):
+    isopen = request.POST['status']
 # Create your views here.
