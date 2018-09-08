239dbf4f02220813e0cfdb321aa18fbb569f95ae
JunwonP <merlin0876@gmail.com>
2018-09-08 23:52:37 +0900

    temperature sensor exmaple

---
+++ temperature sensor/example/example.ino
@@ -0,0 +1,29 @@
+#include <DHT11.h>
+int pin=2;
+DHT11 dht11(pin); 
+void setup()
+{
+   Serial.begin(9600);
+}
+ 
+void loop()
+{
+  int err;
+  float temp, humi;
+  if((err=dht11.read(humi, temp))==0)
+  {
+    Serial.print("temperature:");
+    Serial.print(temp);
+    Serial.print(" humidity:");
+    Serial.print(humi);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println();
+    Serial.print("Error No :");
+    Serial.print(err);
+    Serial.println();    
+  }
+  delay(DHT11_RETRY_DELAY); //delay for reread
+}
